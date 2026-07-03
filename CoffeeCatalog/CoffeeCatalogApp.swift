0ec34fc12ed3b894d94f96dd05769d70f2b3993c
Khrystyna Stadnytska <kstadnytska@softjourn.com>
2026-07-03 12:56:13 +0300

    Initial Commit

---
+++ CoffeeCatalog/CoffeeCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCatalogApp.swift
+//  CoffeeCatalog
+//
+//  Created by Christina Stadnytska on 03.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
